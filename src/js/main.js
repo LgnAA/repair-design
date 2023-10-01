9e1f508772868cc0ee8479ba9dfedca92fea2e3b
LgnAA <you@example.com>
2023-10-01 18:58:17 +0300

    feat: added function close modal window

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  const removeModal = () => modal.classList.remove("modal--visible")
   // const closeBtn = document.querySelectorAll('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
@@ -11,7 +12,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
     element.addEventListener('click', switchModal);
   });
   closeBtn.addEventListener('click', switchModal);
-  // closeBtn.forEach(element => {
-  //   element.addEventListener('click', switchModal);
-  // });
+  document.addEventListener('keydown', (event) => {
+    if (event.code === "Escape") removeModal();
+  }); 
+  document.addEventListener('click', (event) => {
+    if (event.target === modal) removeModal();
+  });
 });
