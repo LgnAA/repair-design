d320ec196f99d28368c78da1544481f2a12b22ad
LgnAA <you@example.com>
2023-10-10 16:30:25 +0300

    feat: validation added

--- src/js/main.js
+++ src/js/main.js
@@ -89,5 +89,74 @@ $(document).ready(function () {
   var bullets = $('.swiper-pagination-horizontal.swiper-pagination-bullets');
   bullets.css('left', prev.width() + 10)
   next.css('left', prev.width() + 10 + bullets.width()+10)
+
+  new WOW().init();
+  // валидация форм
+  function validateForm(form){
+    $(form).validate({
+      errorClass: "invalid",
+      errorElement: "div",
+      rules: {
+        // simple rule, converted to {required:true}
+        userName: {
+          required: true,
+          minlength: 2,
+          maxlength: 15
+        },
+        userPhone: {
+          required: true,
+          minlength: 17
+        },
+        userQuestion: "required",
+        // compound rule
+        userEmail: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        userName: {
+          required: "Заполните поле",
+          minlength: "Слишком короткое имя",
+          maxlength: "Имя не должно превышать 15 символов"
+        },
+        userPhone: {
+          required: "Заполните поле",
+          minlength: "Некорректно введен номер"
+        },
+        userQuestion: "Заполните поле",
+        userEmail: {
+          required: "Заполните поле",
+          email: "Введите Ваш email в формате name@domain.com"
+        }
+      },
+      submitHandler: function (form) {
+        $.ajax({
+          type: "POST",
+          url: "send.php",
+          data: $(form).serialize(),
+          success: function(){
+           $(form)[0].reset();
+            // $(form).find('input').val("");
+            // modalAnswer.toggleClass('modal-answer_visible');
+            // modal.removeClass('modal_visible');
+            // $('.modal-answer__title').text('Спасибо! Заявка успешно отправлена. Наш менеджер перезвонит Вам в течение 15 минут.');
+            // $(form).text('Спасибо! Заявка успешно отправлена. Наш менеджер перезвонит Вам в течение 15 минут.');
+            $(form).html('<p class="modal-answer__text">Спасибо! Заявка успешно отправлена. Наш менеджер перезвонит Вам в течение 15 минут. <br><br>Чтобы узнавать новости первыми, подпишитесь на нашу группу в ВК <br><a class="modal-answer__link" href="https://vk.com/frontend_katrina" target="_blank">Подписаться <img src="./img/vk-icon.svg" alt="vk"></a></p>');
+          },
+          error: function(jqXHR, textStatus) {
+            console.error(jqXHR + " " + textStatus);
+          }
+        });
+      }
+    });
+  }
+  validateForm('.modal__form');
+  validateForm('.control__form');
+  validateForm('.footer__form');
+  
+    // маска для телефона
+    $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
 });
-new WOW().init();
+
+
